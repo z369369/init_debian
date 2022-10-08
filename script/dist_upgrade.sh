3b7f775bfb6108702b2c7b9cac9a0e089ff878cb
lwh <ppp821203@gmail.com>
2022-10-09 05:01:32 +0900

    dist_upgrade

--- script/dist_upgrade.sh
+++ script/dist_upgrade.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 echo "============== [S] Upgrade - ubuntu =============="
-
 sudo do-release-upgrade
-
 echo "============== [E] Upgrade - ubuntu =============="
 
 echo "============== [S] Remove - snap =============="
@@ -12,18 +10,10 @@ sudo systemctl disable snapd.socket
 sudo systemctl disable snapd.seeded.service
 
 sudo dpkg --configure -a
-sudo apt autoremove --purge -y snapd
+sudo apt purge snapd gnome-software-plugin-snap
 
 sudo rm -rf /var/cache/snapd/
 rm -rf ~/snap
-
-sudo apt remove --purge -y \
-    thunderbird gnome-games \
-    rhythmbox totem deja-dup \
-    simple-scan cheese hexchat \
-    aisleriot gnome-{mahjongg,mines,sudoku} sgt-*
-
-#sudo rm -rf /run/timeshift
 
 echo "============== [E] Remove - snap =============="
 
