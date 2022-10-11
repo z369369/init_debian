ef180c854a1bfe213f69bf6a5ee3df8f52606e18
ppp821203 <ppp821203@gmail.com>
2022-10-11 13:17:09 +0900

    fix now

--- 0_all.sh
+++ 0_all.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-./1_delapps_dnf.sh
+./1_delsnap_apt.sh
 ./2_flatpak.sh
 ./3_ppa.sh
 ./4_custom.sh
--- ver_ubuntu/0_all.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-./1_delsnap_apt.sh
-./2_flatpak.sh
-./3_ppa.sh
-./4_custom.sh
-reboot
