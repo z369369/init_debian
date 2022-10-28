e34dcba932e744199760cb95ca9d35b2c0fedde2
ppp821203 <ppp821203@gmail.com>
2022-10-28 09:37:48 +0900

    all

--- 0_all.sh
+++ 0_all.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
+echo "============== [S] Init - Download =============="
+sudo apt install git
+mkdir ~/git
+cd ~/git
+git clone https://github.com/ppp821203/linux_init.git
+chmod -R 777 ~/git
+cd linux_init
+echo "============== [E] Init - Download =============="
+
 ./1_delsnap.sh
 ./2_package.sh
 ./3_ppa.sh
