8f86e6f77f8e07aab8d4a3782cbb1b2acb8ca97d
ppp821203 <ppp821203@gmail.com>
2022-10-28 09:47:24 +0900

    ppa

--- 2_ppa.sh
+++ 2_ppa.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 echo "============== [S] Install - ppa =============="
+sudo apt-get install -y wget gpg
+
+#opera
+wget -qO- https://deb.opera.com/archive.key | sudo apt-key add -
+yes '' | sudo add-apt-repository "deb [arch=i386,amd64] https://deb.opera.com/opera-stable/ stable non-free"
+sudo apt install -y opera-stable
 
 #vscode
-sudo apt-get install -y wget gpg
 wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
 sudo install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg
 sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
@@ -13,11 +18,6 @@ sudo apt install -y code
 wget -q -O - https://dl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
 yes '' | sudo add-apt-repository "deb [arch=amd64] https://dl.google.com/linux/chrome/deb/ stable main" 
 sudo apt install -y google-chrome-stable
-
-#opera
-wget -qO- https://deb.opera.com/archive.key | sudo apt-key add -
-yes '' | sudo add-apt-repository "deb [arch=i386,amd64] https://deb.opera.com/opera-stable/ stable non-free"
-sudo apt install -y opera-stable
 
 ##copyq
 # yes '' | sudo add-apt-repository -y ppa:hluk/copyq
