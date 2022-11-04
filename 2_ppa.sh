21530196fedfd81e4ba0f98f63fe32affa60e51b
ppp821203 <ppp821203@gmail.com>
2022-11-04 09:55:20 +0900

    2ppa

--- 2_ppa.sh
+++ 2_ppa.sh
@@ -23,4 +23,16 @@ sudo apt install -y google-chrome-stable
 # yes '' | sudo add-apt-repository -y ppa:hluk/copyq
 # sudo apt install -y copyq
 
+#firefox
+sudo add-apt-repository ppa:mozillateam/ppa
+
+echo '
+Package: *
+Pin: release o=LP-PPA-mozillateam
+Pin-Priority: 1001
+' | sudo tee /etc/apt/preferences.d/mozilla-firefox 
+
+echo 'Unattended-Upgrade::Allowed-Origins:: "LP-PPA-mozillateam:${distro_codename}";' | sudo tee /etc/apt/apt.conf.d/51unattended-upgrades-firefox
+sudo apt install firefox
+
 echo "============== [E] Install - ppa =============="
