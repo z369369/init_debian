fa834fb603b16e7c9d901dc41825ec128324f22d
ppp821203 <ppp821203@gmail.com>
2023-02-04 22:15:34 +0900

    c

--- script/change_login.sh
+++ script/change_login.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-myfile=$( ls -1 "$HOME/Pictures/BingWallpaper" |  shuf | head -1 )
+img_dir=$HOME/Pictures/BingWallpaper
+img_file=$( ls -1 "$img_dir" |  shuf | head -1 )
 #echo "$HOME/Pictures/BingWallpaper/$myfile"
-echo 1 | sudo -S /home/lwh/script/ubuntu-gdm-set-background --image "$HOME/Pictures/BingWallpaper/$myfile" > /dev/null 2>&1
+echo 1 | sudo -S /home/lwh/script/ubuntu-gdm-set-background --image "$img_dir/$img_file" > /dev/null 2>&1
 #echo 1 | sudo -S /home/lwh/script/ubuntu-gdm-set-background --image "$HOME/Pictures/BingWallpaper/$myfile" 
