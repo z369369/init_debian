28cf2479cd9159e672ebc710943db361406c661c
ppp821203 <ppp821203@gmail.com>
2023-02-08 03:46:14 +0900

    q

--- script/organize.sh
+++ script/organize.sh
@@ -25,6 +25,8 @@ mv *.rar *.zip *.tar *.tar.gz *.7z *.deb ~/Downloads/zip
 # Iso
 mv *.iso ~/Downloads/iso
 
+rm -rf /media/lwh/backup_disk/home/lwh/*
+
 rsync -a --exclude-from /home/lwh --exclude Games --exclude Public --exclude Downloads --exclude .cache --exclude .var  /home/lwh/ /media/lwh/backup_disk/home/lwh/
 
 notify-send 'Complete' 'Organizing your downloads Folder'
