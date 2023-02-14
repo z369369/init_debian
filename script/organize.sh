07815de6225868a637801070324192d6c08fd3c0
ppp821203 <ppp821203@gmail.com>
2023-02-14 21:17:11 +0900

    no over

--- script/organize.sh
+++ script/organize.sh
@@ -25,7 +25,9 @@ mv *.rar *.zip *.tar *.tar.gz *.7z *.deb ~/Downloads/zip
 # Iso
 mv *.iso ~/Downloads/iso
 
-rm -rf /media/lwh/backup_disk/home/lwh/*
+rm -rf /media/lwh/backup_disk/home/lwh
+
+mkdir /media/lwh/backup_disk/home/lwh
 
 rsync -a --exclude-from /home/lwh --exclude Games --exclude Public --exclude Downloads --exclude .cache --exclude .var  /home/lwh/ /media/lwh/backup_disk/home/lwh/
 
