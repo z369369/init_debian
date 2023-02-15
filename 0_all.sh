90ffd50836694b66df36b8d1ced31ad46eeab004
ppp821203 <ppp821203@gmail.com>
2023-02-16 04:28:44 +0900

    fix now

--- 0_all.sh
+++ 0_all.sh
@@ -1,16 +1,13 @@
 #!/bin/bash
-
-#!/bin/bash
-
 echo -e "GET http://google.com HTTP/1.0\n\n" | nc google.com 80 > /dev/null 2>&1
 
 if [ $? -eq 0 ]; then
-    echo "Online"
-    # ./1_remove_unuse.sh
-    # ./2_pkg_custom.sh
-    # ./3_pkg_common.sh
-    # ./4_setting.sh
-    # reboot
+    echo "Online..."
+    ./1_remove_unuse.sh
+    ./2_pkg_custom.sh
+    ./3_pkg_common.sh
+    ./4_setting.sh
+    reboot
 else
-    echo "Offline"
+    echo "Offline... Please check wifi connection"
 fi
