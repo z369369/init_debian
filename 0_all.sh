9db5c247079883b3e0465be76273d03db5b04443
ppp821203 <ppp821203@gmail.com>
2023-02-16 04:26:27 +0900

    1

--- 0_all.sh
+++ 0_all.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
-./1_remove_unuse.sh
-./2_pkg_custom.sh
-./3_pkg_common.sh
-./4_setting.sh
-reboot
+
+#!/bin/bash
+
+echo -e "GET http://google.com HTTP/1.0\n\n" | nc google.com 80 > /dev/null 2>&1
+
+if [ $? -eq 0 ]; then
+    echo "Online"
+    # ./1_remove_unuse.sh
+    # ./2_pkg_custom.sh
+    # ./3_pkg_common.sh
+    # ./4_setting.sh
+    # reboot
+else
+    echo "Offline"
+fi
