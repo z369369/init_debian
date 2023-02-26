33c4d410928264e7a1e55cc69f73e923853e9261
ppp821203 <ppp821203@gmail.com>
2023-02-26 11:27:12 +0900

    obs not work

--- 3_pkg_common.sh
+++ 3_pkg_common.sh
@@ -20,5 +20,6 @@ sudo flatpak install -y $(cat pkglist_flatpak)
 sudo chown -R lwh:lwh ~/.var
 
 #obs not work
+#https://unix.stackexchange.com/questions/552688/is-it-possible-to-roll-back-a-flatpak-update
 sudo flatpak update --commit=694f40fda5aea03f5daf53db094cdcc5c29e62fa507d49d5bcfe399c55809ecd com.obsproject.Studio
 echo "============== [E] Install - flatpak =============="
