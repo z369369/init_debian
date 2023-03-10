e04c3ce743548bf55596af1be0248d1bc90b42f9
ppp821203 <ppp821203@gmail.com>
2023-03-10 10:38:27 +0900

    fix now

--- script/start_wall_login.sh
+++ script/start_wall_login.sh
@@ -2,5 +2,4 @@
 img_dir=$HOME/Pictures/BingWallpaper
 img_file=$( ls -1 "$img_dir" |  shuf | head -1 )
 #echo "$HOME/Pictures/BingWallpaper/$myfile"
-echo 1 | sudo -S /home/lwh/script/ubuntu-gdm-set-background --image "$img_dir/$img_file" > /dev/null 2>&1
-#echo 1 | sudo -S /home/lwh/script/ubuntu-gdm-set-background --image "$HOME/Pictures/BingWallpaper/$myfile" 
+cat ~/.key | sudo -S /home/lwh/script/ubuntu-gdm-set-background --image "$img_dir/$img_file" > /dev/null 2>&1
