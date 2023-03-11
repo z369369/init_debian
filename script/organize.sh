c660fce9f0e294f89ef30f4efbb6597175ca581e
ppp821203 <ppp821203@gmail.com>
2023-03-11 18:58:23 +0900

    org

--- script/organize.sh
+++ script/organize.sh
@@ -31,7 +31,7 @@ mv *.iso ~/Downloads/iso
 #obs not work
 #https://unix.stackexchange.com/questions/552688/is-it-possible-to-roll-back-a-flatpak-update
 #flatpak remote-info --log flathub com.obsproject.Studio
-cat ~/.key | sudo flatpak update --commit=694f40fda5aea03f5daf53db094cdcc5c29e62fa507d49d5bcfe399c55809ecd com.obsproject.Studio
+cat ~/.key | sudo -S flatpak update -y --commit=694f40fda5aea03f5daf53db094cdcc5c29e62fa507d49d5bcfe399c55809ecd com.obsproject.Studio
 
 rsync -a --exclude-from /home/$USER --exclude Phone --exclude Games --exclude Public --exclude Downloads --exclude .cache --exclude .var  /home/$USER/ /media/$USER/backup_disk/home/$USER/
 
