aaab9d89c2b76d3effb9a8469b1f27d7f1a5bfe4
ppp821203 <40662344+ppp821203@users.noreply.github.com>
2023-03-23 05:01:11 +0900

    fix now

--- script/desktop_trust.sh
+++ script/desktop_trust.sh
@@ -2,4 +2,4 @@
 
 for i in ~/Desktop/*.desktop; do    gio set "$i" "metadata::trusted" true ; chmod +x "$i";done
 
-notify-send 'Complete' 'Press F5 for refresh Desktop'
+#notify-send 'Complete' 'Press F5 for refresh Desktop'
