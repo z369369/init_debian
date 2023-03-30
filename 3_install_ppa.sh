68750871eb5d2b5e2d0c0fee5992ccc57915779a
ppp821203 <40662344+ppp821203@users.noreply.github.com>
2023-03-30 20:20:48 +0900

    ppa

--- 3_install_ppa.sh
+++ 3_install_ppa.sh
@@ -7,11 +7,13 @@ wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > pa
 sudo install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg
 sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
 rm -f packages.microsoft.gpg
+sudo apt-get update
 sudo apt install -y code
 
 #chrome
 wget -q -O - https://dl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://dl.google.com/linux/chrome/deb/ stable main"
+sudo apt-get update
 sudo apt install -y google-chrome-stable
 
 #firefox
@@ -29,6 +31,7 @@ Pin: release o=Ubuntu*
 Pin-Priority: -1
 ' | sudo tee /etc/apt/preferences.d/firefox_no_snap
 
+sudo apt-get update
 sudo apt install firefox
 
 #kernel
