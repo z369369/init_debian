961b109a42505d58f6cb76970c570891899a033b
ppp821203 <40662344+ppp821203@users.noreply.github.com>
2023-03-30 20:36:41 +0900

    install

--- 3_install_ppa.sh
+++ 3_install_ppa.sh
@@ -4,14 +4,6 @@ sudo apt-get update && sudo apt-get -y upgrade
 sudo apt -y autoremove
 sudo apt-get install -y wget gpg
 
-#vscode
-wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
-sudo install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg
-sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
-rm -f packages.microsoft.gpg
-sudo apt-get update && sudo apt-get -y upgrade
-sudo apt install -y code
-
 #chrome
 wget -q -O - https://dl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://dl.google.com/linux/chrome/deb/ stable main"
@@ -19,8 +11,6 @@ sudo apt-get update && sudo apt-get -y upgrade
 sudo apt install -y google-chrome-stable
 
 #firefox
-sudo add-apt-repository ppa:mozillateam/ppa
-
 echo '
 Package: snapd
 Pin: release a=*
@@ -33,8 +23,19 @@ Pin: release o=Ubuntu*
 Pin-Priority: -1
 ' | sudo tee /etc/apt/preferences.d/firefox_no_snap
 
+sudo add-apt-repository ppa:mozillateam/ppa
 sudo apt-get update && sudo apt-get -y upgrade
 sudo apt install firefox
+
+#vscode
+wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
+sudo install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg
+sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
+rm -f packages.microsoft.gpg
+sudo apt-get update && sudo apt-get -y upgrade
+sudo apt install -y code
+
+sudo apt -y autoremove
 
 #kernel
 # sudo add-apt-repository ppa:cappelikan/ppa
--- 4_install_apt.sh
+++ 4_install_apt.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 echo "============== [S] 4.Install - apt =============="
-
 sudo apt-get update && sudo apt-get -y upgrade
 sudo apt-get install -y $(cat pkg_install_apt)
 sudo apt -y autoremove
