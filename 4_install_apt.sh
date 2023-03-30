578a4a3f6e4b85b0563f5108b0a527e0bac49d01
ppp821203 <40662344+ppp821203@users.noreply.github.com>
2023-03-30 19:59:52 +0900

    4

--- 4_install_apt.sh
+++ 4_install_apt.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "============== [S] 4.Install - apt =============="
-
+sudo apt update
 sudo apt-get install -y $(cat pkg_install_apt)
 sudo apt-get update && sudo apt-get -y upgrade
 sudo apt autoremove
