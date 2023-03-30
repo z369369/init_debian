74e9750ca670be4e6124d750befd7235393f7476
ppp821203 <40662344+ppp821203@users.noreply.github.com>
2023-03-30 23:07:01 +0900

    apt upgrade

--- 4_install_apt.sh
+++ 4_install_apt.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 echo "============== [S] 4.Install - apt =============="
+
 sudo apt-get update
 sudo apt-get autoremove
 sudo apt-get autoclean
 sudo apt-get -y upgrade
 
 sudo apt install -y $(cat pkg_install_apt)
+
 sudo apt -y autoremove
 sudo deborphan | xargs sudo apt -y remove --purge
 
