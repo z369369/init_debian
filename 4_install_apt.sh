90351539e8177fcdc1059e41f1306e15af8609d1
ppp821203 <40662344+ppp821203@users.noreply.github.com>
2023-03-30 20:07:36 +0900

    1

--- 4_install_apt.sh
+++ 4_install_apt.sh
@@ -3,7 +3,7 @@ echo "============== [S] 4.Install - apt =============="
 
 sudo apt-get update && sudo apt-get -y upgrade
 sudo apt-get install -y $(cat pkg_install_apt)
-sudo apt autoremove
+sudo apt -y autoremove
 sudo deborphan | xargs sudo apt-get -y remove --purge
 
 echo "============== [E] 4.Install - apt =============="
