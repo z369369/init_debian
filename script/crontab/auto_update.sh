15f5ca8f47f022922e1fc6cbb3a25d2877a3dca7
ppp821203 <40662344+ppp821203@users.noreply.github.com>
2023-04-02 22:23:55 +0900

    auto

--- script/crontab/auto_update.sh
+++ script/crontab/auto_update.sh
@@ -3,3 +3,6 @@
 flatpak update --noninteractive --assumeyes
 
 cat ~/.key | sudo -S sudo apt update && sudo apt -y upgrade 
+
+cat ~/.key | sudo -S sudo apt-get install --only-upgrade `sudo apt-get upgrade | awk 'BEGIN{flag=0} /The following packages have been kept back:/ { flag=1} /^ /{if (flag) print}'`
+cat ~/.key | sudo -S sudo apt-get install --only-upgrade `sudo apt-get upgrade | awk 'BEGIN{flag=0} /다음 패키지를 과거 버전으로 유지합니다:/ { flag=1} /^ /{if (flag) print}'`
