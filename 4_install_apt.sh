ddb3d0ffc70fb9bd6a2642f6d075461f2ac99ded
ppp821203 <40662344+ppp821203@users.noreply.github.com>
2023-04-02 20:38:24 +0900

    4

--- 4_install_apt.sh
+++ 4_install_apt.sh
@@ -9,6 +9,8 @@ sudo apt-get -y upgrade
 sudo apt-get install --only-upgrade `sudo apt-get upgrade | awk 'BEGIN{flag=0} /The following packages have been kept back:/ { flag=1} /^ /{if (flag) print}'`
 sudo apt-get install --only-upgrade `sudo apt-get upgrade | awk 'BEGIN{flag=0} /다음 패키지를 과거 버전으로 유지합니다:/ { flag=1} /^ /{if (flag) print}'`
 
+sudo apt update
+
 sudo apt install -y $(cat pkg_install_apt)
 
 sudo apt -y autoremove
