4489fa11ed0eb9a4442fbeab8929b82da9d5a905
ppp821203 <40662344+ppp821203@users.noreply.github.com>
2023-04-02 20:21:57 +0900

    ppa

--- 3_install_ppa.sh
+++ 3_install_ppa.sh
@@ -5,10 +5,10 @@ sudo apt -y autoremove
 sudo apt install -y wget gpg
 
 #chrome
-curl -fSsL https://dl.google.com/linux/linux_signing_key.pub | gpg --dearmor | sudo tee /usr/share/keyrings/google-chrome.gpg > /dev/null
-echo deb [arch=amd64 signed-by=/usr/share/keyrings/google-chrome.gpg] http://dl.google.com/linux/chrome/deb/ stable main | sudo tee /etc/apt/sources.list.d/google-chrome.list
-sudo apt update && sudo apt -y upgrade
-sudo apt install -y google-chrome-stable
+# curl -fSsL https://dl.google.com/linux/linux_signing_key.pub | gpg --dearmor | sudo tee /usr/share/keyrings/google-chrome.gpg > /dev/null
+# echo deb [arch=amd64 signed-by=/usr/share/keyrings/google-chrome.gpg] http://dl.google.com/linux/chrome/deb/ stable main | sudo tee /etc/apt/sources.list.d/google-chrome.list
+# sudo apt update && sudo apt -y upgrade
+# sudo apt install -y google-chrome-stable
 
 #firefox
 echo '
