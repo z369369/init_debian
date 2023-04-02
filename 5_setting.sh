05fb337a5a31b6fce688bdafea39785b0bc734c8
ppp821203 <40662344+ppp821203@users.noreply.github.com>
2023-04-02 20:24:35 +0900

    6

--- 5_setting.sh
+++ 5_setting.sh
@@ -74,17 +74,6 @@ sudo ufw reload
 
 # git clone https://github.com/cfgnunes/nautilus-scripts.git ~/.local/share/nautilus/scripts
 
-echo "============== [S] Syncthing Install =============="
-
-sudo curl -s -o /usr/share/keyrings/syncthing-archive-keyring.gpg https://syncthing.net/release-key.gpg
-echo "deb [signed-by=/usr/share/keyrings/syncthing-archive-keyring.gpg] https://apt.syncthing.net/ syncthing stable" | sudo tee /etc/apt/sources.list.d/syncthing.list
-sudo apt update
-sudo apt install syncthing	
-sudo systemctl enable syncthing@$USER.service
-sudo systemctl start syncthing@$USER.service
-
-echo "============== [E] Syncthing Install =============="
-
 
 echo "============== [S] 5.Set Sudo key file =============="
 
---
+++ 6_test_ppa.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+echo "============== [S] Syncthing Install =============="
+
+sudo curl -s -o /usr/share/keyrings/syncthing-archive-keyring.gpg https://syncthing.net/release-key.gpg
+echo "deb [signed-by=/usr/share/keyrings/syncthing-archive-keyring.gpg] https://apt.syncthing.net/ syncthing stable" | sudo tee /etc/apt/sources.list.d/syncthing.list
+sudo apt update
+sudo apt install syncthing	
+sudo systemctl enable syncthing@$USER.service
+sudo systemctl start syncthing@$USER.service
+
+echo "============== [E] Syncthing Install =============="
+
+
+echo "============== [S] Chrome Install =============="
+
+chrome
+curl -fSsL https://dl.google.com/linux/linux_signing_key.pub | gpg --dearmor | sudo tee /usr/share/keyrings/google-chrome.gpg > /dev/null
+echo deb [arch=amd64 signed-by=/usr/share/keyrings/google-chrome.gpg] http://dl.google.com/linux/chrome/deb/ stable main | sudo tee /etc/apt/sources.list.d/google-chrome.list
+sudo apt update && sudo apt -y upgrade
+sudo apt install -y google-chrome-stable
+
+echo "============== [E] Chrome Install =============="
