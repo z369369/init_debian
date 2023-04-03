3bf337c9db69ffcbbf66a258de19814ca72cf516
ppp821203 <40662344+ppp821203@users.noreply.github.com>
2023-04-04 01:57:33 +0900

    fixed

--- script/startup/wallpaper.sh
+++ script/startup/wallpaper.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-img_dir=$HOME/Pictures/BingWallpaper
+img_dir=$HOME/.local/share/BingWallpaper
 img_file=$( ls -1 "$img_dir" |  shuf | head -1 )
 #echo "$HOME/Pictures/BingWallpaper/$myfile"
 cat ~/.key | sudo -S /home/$USER/script/ubuntu-gdm-set-background --image "$img_dir/$img_file" > /dev/null 2>&1
