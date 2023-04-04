6d786a50d9f2c1e31e14496c8ee6c0af821514fa
ppp821203 <40662344+ppp821203@users.noreply.github.com>
2023-04-05 04:34:27 +0900

    trash

--- 1_remove_trash.sh
+++ 1_remove_trash.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 echo "============== [S] 1.Remove - Snap =============="
 
-sudo systemctl disable NetworkManager-wait-online.service
-
 sudo systemctl disable snapd.service
 sudo systemctl disable snapd.socket
 sudo systemctl disable snapd.seeded.service
@@ -30,3 +28,11 @@ echo "============== [S] 1.Remove - Ubuntu default extension =============="
 sudo rm -rf /usr/share/gnome-shell/extensions/*
 
 echo "============== [S] 1.Remove - Ubuntu default extension =============="
+
+
+
+echo "============== [S] 1.Remove Service =============="
+
+sudo systemctl disable NetworkManager-wait-online.service
+
+echo "============== [E] 1.Remove Service =============="
