d87de20554a33002101ffcc8a2fbd7f9cae2248a
ppp821203 <40662344+ppp821203@users.noreply.github.com>
2023-04-04 09:04:37 +0900

    trust

--- script/desktop/trust.sh
+++ script/desktop/trust.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
+v_path='/home/lwh/.local/share/applications'
+cd $v_path
+for i in $v_path/*.desktop; do    gio set "$i" "metadata::trusted" true ; chmod +x "$i";done
 
-cd /home/$USER/Desktop
 
-for i in /home/$USER/Desktop/*.desktop; do    gio set "$i" "metadata::trusted" true ; chmod +x "$i";done
+v_path='/home/lwh/Desktop'
+cd $v_path
+for i in $v_path/*.desktop; do    gio set "$i" "metadata::trusted" true ; chmod +x "$i";done
 
-touch /home/$USER/Desktop
+
+touch /home/lwh/Desktop
 
 #notify-send 'Complete' 'Press F5 for refresh Desktop'
