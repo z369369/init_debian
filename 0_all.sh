31925d554274e531bb09a5f19a1a206fd643e6d8
ppp821203 <40662344+ppp821203@users.noreply.github.com>
2023-04-05 00:35:10 +0900

    change some text

--- 0_all.sh
+++ 0_all.sh
@@ -3,13 +3,13 @@ echo -e "GET http://google.com HTTP/1.0\n\n" | nc google.com 80 > /dev/null 2>&1
 
 if [ $? -eq 0 ]; then
     echo "Start custom installation"
-    ./1_remove_unuse.sh
+    ./1_remove_trash.sh
     ./2_telemetry_off.sh
     ./3_install_apt.sh
     ./3_install_apt.sh
     ./4_install_flatpak.sh
-    ./5_setting.sh
-    ./6_install_ppa.sh
+    ./5_custom_setting.sh
+    ./6_custom_install.sh
     reboot
 else
     echo "Please check Internet connection"
