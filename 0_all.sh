1a3ca4b6d1982135bf7809dd77453c442c962f4b
ppp821203 <40662344+ppp821203@users.noreply.github.com>
2023-04-04 23:35:03 +0900

    all

--- 0_all.sh
+++ 0_all.sh
@@ -2,7 +2,7 @@
 echo -e "GET http://google.com HTTP/1.0\n\n" | nc google.com 80 > /dev/null 2>&1
 
 if [ $? -eq 0 ]; then
-    echo "Online..."
+    echo "Start custom installation"
     ./1_remove_unuse.sh
     ./2_telemetry_off.sh
     ./3_install_apt.sh
@@ -12,5 +12,5 @@ if [ $? -eq 0 ]; then
     ./6_install_ppa.sh
     reboot
 else
-    echo "Offline... Please check Internet connection"
+    echo "Please check Internet connection"
 fi
