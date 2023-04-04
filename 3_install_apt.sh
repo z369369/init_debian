92cd80c79e31c4b9d741ac227357ff16cd963c73
ppp821203 <40662344+ppp821203@users.noreply.github.com>
2023-04-04 23:05:40 +0900

    pkg

--- 3_install_apt.sh
+++ 3_install_apt.sh
@@ -9,7 +9,8 @@ sudo apt-get -y upgrade
 sudo apt-get install --only-upgrade `sudo apt-get upgrade | awk 'BEGIN{flag=0} /The following packages have been kept back:/ { flag=1} /^ /{if (flag) print}'`
 sudo apt-get install --only-upgrade `sudo apt-get upgrade | awk 'BEGIN{flag=0} /다음 패키지를 과거 버전으로 유지합니다:/ { flag=1} /^ /{if (flag) print}'`
 
-sudo apt install -y $(cat pkg_install_apt)
+# sudo apt install -y $(cat pkg_install_apt)
+sudo apt install -y $(grep -vE "^\s*#" pkg_install_apt | sed -e 's/#.*//'  | tr "\n" " ")
 
 sudo apt -y autoremove
 sudo deborphan | xargs sudo apt -y remove --purge
