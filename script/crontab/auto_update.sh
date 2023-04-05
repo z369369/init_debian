601e92df51db716cb268fa61c4f18d393531b434
ppp821203 <40662344+ppp821203@users.noreply.github.com>
2023-04-06 01:14:53 +0900

    update

--- script/crontab/auto_update.sh
+++ script/crontab/auto_update.sh
@@ -6,3 +6,6 @@ cat /home/lwh/.key | sudo -S sudo apt update && sudo apt -y upgrade
 
 cat /home/lwh/.key | sudo -S sudo apt-get install --only-upgrade `sudo apt-get upgrade | awk 'BEGIN{flag=0} /The following packages have been kept back:/ { flag=1} /^ /{if (flag) print}'`
 cat /home/lwh/.key | sudo -S sudo apt-get install --only-upgrade `sudo apt-get upgrade | awk 'BEGIN{flag=0} /다음 패키지를 과거 버전으로 유지합니다:/ { flag=1} /^ /{if (flag) print}'`
+
+
+flatpak uninstall --unused --noninteractive --assumeyes
