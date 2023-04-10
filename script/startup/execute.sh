337c94f3448b3a28ebd8149ebd00a19f03917cb1
ppp821203 <40662344+ppp821203@users.noreply.github.com>
2023-04-11 08:24:16 +0900

    pref

--- script/startup/execute.sh
+++ script/startup/execute.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 sleep 5s
-/usr/bin/bleachbit -c --preset 
 gnome-terminal
 nautilus ~
 #/usr/bin/google-chrome
--- script/startup/wallpaper.sh
+++ script/startup/wallpaper.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+
+cat ~/.key | sudo -S systemctl restart bluetooth.service
+
 img_dir=$HOME/.local/share/BingWallpaper
 img_file=$( ls -1 "$img_dir" |  shuf | head -1 )
 #echo "$HOME/Pictures/BingWallpaper/$myfile"
