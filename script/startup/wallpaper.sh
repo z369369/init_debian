02d4af9f8b8fa8e5ce2581c6deed37b5ccec126f
ppp821203 <40662344+ppp821203@users.noreply.github.com>
2023-04-12 10:00:33 +0900

    change path

--- script/startup/wallpaper.sh
+++ script/startup/wallpaper.sh
@@ -5,4 +5,4 @@ cat ~/.key | sudo -S systemctl restart bluetooth.service
 img_dir=$HOME/.local/share/BingWallpaper
 img_file=$( ls -1 "$img_dir" |  shuf | head -1 )
 #echo "$HOME/Pictures/BingWallpaper/$myfile"
-cat ~/.key | sudo -S /home/$USER/script/ubuntu-gdm-set-background --image "$img_dir/$img_file" > /dev/null 2>&1
+cat ~/.key | sudo -S /home/$USER/script/startup/ubuntu-gdm-set-background --image "$img_dir/$img_file" > /dev/null 2>&1
