741605127e1bc6801ae0c881fade9fe223fde8cb
ppp821203 <40662344+ppp821203@users.noreply.github.com>
2023-04-20 22:43:48 +0900

    7

--- 7_setting_firewall.sh
+++ 7_setting_firewall.sh
@@ -2,39 +2,9 @@
 sudo ufw default deny incoming
 sudo ufw default deny outgoing
 
-#gsconnect
-sudo ufw allow 1714:1764/tcp
-sudo ufw allow out 1714:1764/tcp
-
-sudo ufw allow 1714:1764/udp
-sudo ufw allow out 1714:1764/udp
-
-#RDP
-sudo ufw allow 3389
-sudo ufw allow out 3389
-
-#common ssl port
-sudo ufw allow 8443
-sudo ufw allow out 8443
-
-#multicast dns - game
-sudo ufw allow 5353
-sudo ufw allow out 5353
-
-#vnc
-sudo ufw allow 5900
-sudo ufw allow out 5900
-
-#syncthing
-sudo ufw allow 22000
-sudo ufw allow out 22000
-
-sudo ufw allow 21027
-sudo ufw allow out 21027
-
-#game streaming port
-sudo ufw allow 9663
-sudo ufw allow out 9663
+#local network possible
+sudo ufw allow from 192.168.31.0/24
+sudo ufw allow out from 192.168.31.0/24
 
 #http
 sudo ufw allow 80
@@ -44,21 +14,17 @@ sudo ufw allow out 80
 sudo ufw allow 443
 sudo ufw allow out 443
 
-#DNS / APT install port
-sudo ufw allow 53
-sudo ufw allow out 53
+#common ssl port
+sudo ufw allow 8443
+sudo ufw allow out 8443
 
-#DHCP port
-sudo ufw allow 67
-sudo ufw allow out 67
+#game streaming port
+sudo ufw allow 9663
+sudo ufw allow out 9663
 
-#network timer
-sudo ufw allow 123
-sudo ufw allow out 123
-
-#wifi router
-sudo ufw allow 5351
-sudo ufw allow out 5351
+#syncthing
+sudo ufw allow 21027
+sudo ufw allow out 21027
 
 #microsoft game pass
 sudo ufw allow 1030:1099/udp
