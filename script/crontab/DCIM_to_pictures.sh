99cf0741b672f153b3274b4a62e6b583d5fb240b
ppp821203 <40662344+ppp821203@users.noreply.github.com>
2023-04-27 16:20:11 +0900

    fix dcim

--- script/crontab/DCIM_to_pictures.sh
+++ script/crontab/DCIM_to_pictures.sh
@@ -4,8 +4,9 @@ rsync -a --exclude-from ~/phone/DCIM --exclude .stf* ~/phone/DCIM/ ~/Pictures/
 #sleep 20s
 
 #find ~/phone/DCIM -type f -exec rm -f {} \;
-fdupes -rdN ~/Pictures/ ~/phone/DCIM/
+fdupes -irdN -o time ~/Pictures/ ~/phone/DCIM/
 
-fdupes -rdN ~/phone/000_Personal/Attachment/ ~/Pictures/
+fdupes -irdN -o time ~/phone/000_Personal/Attachment/ ~/Pictures/
 
-#fdupes는 앞에 파라미터는 남기고, 뒤에 파라미터 항목은 지운다
+#fdupes는 옵션 order by time asc, i는 reverse
+#-i와 -o time 함께 쓰면 최신 것만 남기겠다(time desc)
