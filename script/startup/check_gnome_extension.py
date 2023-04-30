f39f82545106a41c261576790f9d95a4601b69de
ppp821203 <40662344+ppp821203@users.noreply.github.com>
2023-04-30 18:25:08 +0900

    check gnome

---
+++ script/startup/check_gnome_extension.py
@@ -0,0 +1,76 @@
+import json, os, shutil, time, requests, re, os
+from datetime import datetime
+from bs4 import BeautifulSoup
+
+if __name__ == '__main__':
+    ext_path = '/home/lwh/git/linux_init/gnome-shell/extensions'
+    a_ext_list = os.listdir(ext_path)
+    a_version_all = []
+    for item in a_ext_list:
+        meta_file = ext_path + '/' + item + '/' + 'metadata.json'
+        with open(meta_file, 'r') as f:
+            data = json.load(f)
+            a_item_version = data['shell-version']
+            # Merge the arrays
+            a_version_all = a_version_all + a_item_version
+            
+    # Convert the array to a set.
+    a_version_all = set(a_version_all)
+
+    # Convert the set back to a list.
+    a_version_all = list(a_version_all)    
+    a_version_all.sort()
+    a_version_all.reverse()
+    a_version_all = a_version_all[:5]
+    
+    # print(a_version_all)
+    item_title = '|COL|'
+    item_cotnent_save = ''
+
+    for item in a_ext_list:
+        meta_file = ext_path + '/' + item + '/' + 'metadata.json'
+        item_title = item_title + item + '|'
+        item_cotnent = '|' + item[:20] + '|'
+        
+        with open(meta_file, 'r') as f:
+            data = json.load(f)
+            a_item_version = data['shell-version']
+            
+            for a_ver in a_version_all:
+                b_compa = ''
+                if a_ver in a_item_version:
+                    b_compa = 'O'
+
+                item_cotnent = item_cotnent + b_compa + '|'
+
+        item_cotnent_save = item_cotnent_save + item_cotnent + '\n'
+
+
+    p_title = '|-|'
+    for item in a_version_all:
+        p_title = p_title + item + '|' 
+        
+    p_con = p_title + '\n'
+    p_con = p_con + '|--|---|---|---|---|---|' + '\n'
+    p_con = p_con + item_cotnent_save
+
+    md_title = 'Linux GNOME Extension Compability'
+
+    now = datetime.now()
+    md_title_key = now.strftime("%Y-%m-%d %H:%M:%S")
+
+    s_out = ''
+    s_out = s_out + '---\r\n'
+    s_out = s_out + 'date: ' + md_title_key + '\r\n'
+    s_out = s_out + 'status: \r\n'
+    s_out = s_out + 'alias: \r\n'
+    s_out = s_out + 'tags: Linux\r\n'
+    s_out = s_out + 'subs: Extension\r\n'
+    s_out = s_out + '---\r\n'
+    s_out = s_out + '\r\n'
+    s_out = s_out + "# " + md_title + "\r\n\r\n"
+    s_out = s_out + " GNOME 확장 프로그램 익스텐션 사용 가능 여부 확인 \r\n\r\n"
+    s_out = s_out + p_con 
+    
+    with open('/home/lwh/phone/000_Personal/Linux/%s.md' % (md_title), 'w', encoding="utf-8") as out_file:
+        out_file.write(s_out)
--- script/startup/execute.sh
+++ script/startup/execute.sh
@@ -3,6 +3,7 @@ sleep 5s
 cat ~/.key | sudo -S ufw default deny outgoing > /dev/null
 /home/lwh/script/startup/organize_startup.sh
 /home/lwh/script/startup/fdupes_pictures.sh
+python3 /home/lwh/script/startup/check_gnome_extension.py
 
 gnome-terminal
 nautilus ~
