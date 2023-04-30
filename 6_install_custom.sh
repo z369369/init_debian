039a368da59956eebe077fb8c71118be483c89d7
ppp821203 <40662344+ppp821203@users.noreply.github.com>
2023-04-30 10:25:23 +0900

    brave

--- 6_install_custom.sh
+++ 6_install_custom.sh
@@ -61,3 +61,12 @@ sudo apt update && sudo apt -y upgrade
 sudo apt install -y google-chrome-stable
 
 echo "============== [E] 6.Install - Chrome =============="
+
+
+echo "============== [S] 6.Install - Brave =============="
+sudo apt install curl
+sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
+echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
+sudo apt update
+sudo apt install brave-browser
+echo "============== [E] 6.Install - Brave =============="
