46015b3df98771d360b89322c299c07dd71f19eb
ppp821203 <40662344+ppp821203@users.noreply.github.com>
2023-05-01 15:33:14 +0900

    fix baobab

--- script/startup/execute.sh
+++ script/startup/execute.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-sleep 5s
-cat ~/.key | sudo -S ufw default deny outgoing > /dev/null
 /home/lwh/script/startup/organize_startup.sh
 /home/lwh/script/startup/fdupes_pictures.sh
 python3 /home/lwh/script/startup/check_gnome_extension.py
+cat ~/.key | sudo -S ufw default deny outgoing 
 
 gnome-terminal
-nautilus ~
-#/usr/bin/google-chrome
-firefox &
+nohup nautilus ~ &
+nohup firefox &
+disown
 
+exit 0
