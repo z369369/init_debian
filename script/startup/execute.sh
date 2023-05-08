d52d3e232b5ad9f5b3efc7078f565ea7bd2fe1f8
ppp821203 <40662344+ppp821203@users.noreply.github.com>
2023-05-08 23:27:42 +0900

    ignore

--- script/startup/execute.sh
+++ script/startup/execute.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 /home/lwh/script/startup/organize_startup.sh
 /home/lwh/script/startup/fdupes_pictures.sh
-python3 /home/lwh/script/startup/check_gnome_extension.py
+python3 /home/lwh/script/py/check_gnome_extension.py
 cat ~/.key | sudo -S ufw default deny outgoing 
 
 
