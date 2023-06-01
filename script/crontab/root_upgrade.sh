32551358f50362df067a80fd0e014a64c08c6e19
ppp821203 <40662344+ppp821203@users.noreply.github.com>
2023-06-02 07:31:44 +0900

    remove mainline

--- script/crontab/root_upgrade.sh
+++ script/crontab/root_upgrade.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-cat /home/lwh/.key | sudo -S sudo apt update && sudo apt -y upgrade --only-upgrade firefox code
-
+cat /home/lwh/.key | sudo -S sudo apt update && sudo apt-get -y install --only-upgrade firefox code
 #cat /home/lwh/.key | sudo -S sudo apt update && sudo apt -y upgrade 
 #cat /home/lwh/.key | sudo -S sudo apt-get install --only-upgrade `sudo apt-get upgrade | awk 'BEGIN{flag=0} /The following packages have been kept back:/ { flag=1} /^ /{if (flag) print}'`
 #cat /home/lwh/.key | sudo -S sudo apt-get install --only-upgrade `sudo apt-get upgrade | awk 'BEGIN{flag=0} /다음 패키지를 과거 버전으로 유지합니다:/ { flag=1} /^ /{if (flag) print}'`
