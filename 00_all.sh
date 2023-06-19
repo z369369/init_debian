ad9e89f19329cf227a7e95b5516487fb7924394e
ppp821203 <40662344+ppp821203@users.noreply.github.com>
2023-06-20 04:28:40 +0900

    fix script

--- 00_all.sh
+++ 00_all.sh
@@ -3,35 +3,39 @@
 E_NOTROOT=87 # Non-root exit error.
 
 ## check if is sudoer
+echo "[1/3]"
 if ! $(sudo -l &> /dev/null); then
-    echo "[1/3] root privileges are needed to run this script"
+    echo "  root privileges are needed to run this script"
     echo "  User lwh should be in a sudo user group"
     echo ""
     echo "  su -"
     echo "  adduser lwh sudo"
     echo "  su - lwh"
-    echo "  "
+    echo ""
+    echo "  [FAIL] check upper message"
     exit $E_NOTROOT
 else 
-    echo "[1/3] Welcome sudo group user..."
+    echo "  Welcome sudo group user..."
 fi
 
+echo "[2/3]"
 if [[ $(whoami) == "lwh" ]]; then
-    echo "[2/3] Welcome lwh..."
+    echo "  Welcome lwh..."
 else
-    echo "[2/3] You are not lwh"
+    echo "  [FAIL] You are not lwh"
     exit
 fi
 
+echo "[3/3]" 
 echo -e "GET http://google.com HTTP/1.0\n\n" | nc google.com 80 > /dev/null 2>&1
 
 if [ $? -eq 0 ]; then
-    echo "[3/3] Start custom installation..."
+    echo "  Start custom installation..."
     ./11_remove_trash.sh
     ./13_install_apt.sh
     ./17_setting_firewall.sh
     ./34_install_flatpak.sh
     ./35_setting_custom.sh    
 else
-    echo "[3/3] Please check Internet connection"
+    echo "  [FAIL] Please check Internet connection"
 fi
