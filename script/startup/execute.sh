04c7c9998a092485bba889c134ce0c54eb9f1aec
ppp821203 <40662344+ppp821203@users.noreply.github.com>
2023-06-21 04:58:52 +0900

    move error terminal_nautilus

--- script/startup/execute.sh
+++ script/startup/execute.sh
@@ -6,8 +6,12 @@ cat ~/.key | sudo -S ufw default deny outgoing
 cat ~/.key | sudo -S systemctl start bluetooth.service
 cat ~/.key | sudo -S ufw enable
 
+wmctrl -s 4
 gnome-terminal
+
+wmctrl -s 3
 nohup nautilus ~ 1> /dev/null 2>&1 &
+
 nohup firefox-esr 1> /dev/null 2>&1 &
 nohup syncthing --allow-newer-config --no-browser 1> /dev/null 2>&1 &
 disown
