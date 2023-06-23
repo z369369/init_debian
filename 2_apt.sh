6a0eb41b6e635fcaf5215f0e75e6ad0343354dd9
ppp821203 <40662344+ppp821203@users.noreply.github.com>
2023-06-23 12:25:28 +0900

    fix to get

--- 2_apt.sh
+++ 2_apt.sh
@@ -9,7 +9,7 @@ sudo apt -y upgrade
 #sudo apt install --only-upgrade `apt upgrade | awk 'BEGIN{flag=0} /The following packages have been kept back:/ { flag=1} /^ /{if (flag) print}'`
 #sudo apt install --only-upgrade `apt upgrade | awk 'BEGIN{flag=0} /다음 패키지를 과거 버전으로 유지합니다:/ { flag=1} /^ /{if (flag) print}'`
 
-sudo apt install -y $(grep -vE "^\s*#" install.list | sed -e 's/#.*//'  | tr "\n" " " | cut -d '*' -f 1)
+sudo apt-get install -y $(grep -vE "^\s*#" install.list | sed -e 's/#.*//'  | tr "\n" " " | cut -d '*' -f 1)
 
 sudo apt -y autoremove
 
