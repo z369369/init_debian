578227c9691affdfbb9010214b98ec86ea4a3f47
ppp821203 <40662344+ppp821203@users.noreply.github.com>
2023-06-27 22:59:45 +0900

    apt

--- 2_apt.sh
+++ 2_apt.sh
@@ -6,14 +6,6 @@ sudo apt -y autoremove
 sudo apt autoclean
 sudo apt -y upgrade
 
-#vscode
-wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
-sudo install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg
-sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
-rm -f packages.microsoft.gpg
-sudo apt update && sudo apt -y upgrade
-sudo apt install -y code
-
 #sudo apt install --only-upgrade `apt upgrade | awk 'BEGIN{flag=0} /The following packages have been kept back:/ { flag=1} /^ /{if (flag) print}'`
 #sudo apt install --only-upgrade `apt upgrade | awk 'BEGIN{flag=0} /다음 패키지를 과거 버전으로 유지합니다:/ { flag=1} /^ /{if (flag) print}'`
 
@@ -25,9 +17,23 @@ echo "============== [E] [ 2-1 / 6 ] Install - apt =============="
 
 echo "============== [S] [ 2-2 / 6 ] Install - apt =============="
 
+#vscode - https://wiki.debian.org/VisualStudioCode
+curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
+sudo install -o root -g root -m 644 microsoft.gpg /usr/share/keyrings/microsoft-archive-keyring.gpg
+sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/usr/share/keyrings/microsoft-archive-keyring.gpg] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
+
+sudo apt-get install apt-transport-https
+sudo apt-get update
+sudo apt-get install code # or code-insiders
+
+echo "============== [E] [ 2-2 / 6 ] Install - apt =============="
+
+
+echo "============== [S] [ 2-3 / 6 ] Install - apt =============="
+
 sudo apt update
 sudo apt install -y flatpak
 # sudo apt install -y gnome-software gnome-software-plugin-flatpak
 
-echo "============== [E] [ 2-2 / 6 ] Install - apt =============="
+echo "============== [E] [ 2-3 / 6 ] Install - apt =============="
 
