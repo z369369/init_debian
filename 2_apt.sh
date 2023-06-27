43ab5e04636596f02af0ec8421be257cfd5628d4
ppp821203 <40662344+ppp821203@users.noreply.github.com>
2023-06-27 11:43:23 +0900

    apt upgrade

--- 2_apt.sh
+++ 2_apt.sh
@@ -6,6 +6,14 @@ sudo apt -y autoremove
 sudo apt autoclean
 sudo apt -y upgrade
 
+#vscode
+wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
+sudo install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg
+sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
+rm -f packages.microsoft.gpg
+sudo apt update && sudo apt -y upgrade
+sudo apt install -y code
+
 #sudo apt install --only-upgrade `apt upgrade | awk 'BEGIN{flag=0} /The following packages have been kept back:/ { flag=1} /^ /{if (flag) print}'`
 #sudo apt install --only-upgrade `apt upgrade | awk 'BEGIN{flag=0} /다음 패키지를 과거 버전으로 유지합니다:/ { flag=1} /^ /{if (flag) print}'`
 
