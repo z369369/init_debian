3c69bd34e2598088c4bd84facc4a17031d3b8483
ppp821203 <40662344+ppp821203@users.noreply.github.com>
2023-07-15 06:33:39 +0900

    upgrade

--- script/startup/execute.sh
+++ script/startup/execute.sh
@@ -3,14 +3,14 @@
 ~/script/startup/fdupes_pictures.sh
 #~/script/startup/wallpaper.sh
 #python3 ~/script/py/startup_gnome_extension.py
-cat ~/.key | sudo -S ufw default deny outgoing 
 cat ~/.key | sudo -S systemctl start bluetooth.service
+cat ~/.key | sudo -S ufw default deny outgoing 
 cat ~/.key | sudo -S ufw enable
 
-wmctrl -s 4
+#wmctrl -s 4
 gnome-terminal
 
-wmctrl -s 3
+#wmctrl -s 3
 nohup nautilus ~ 1> /dev/null 2>&1 &
 nohup firefox-esr 1> /dev/null 2>&1 &
 #nohup syncthing --allow-newer-config --no-browser 1> /dev/null 2>&1 &
