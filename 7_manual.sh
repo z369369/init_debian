32d09103a3c11f7868fee3e855c54a98cbcc9277
ppp821203 <40662344+ppp821203@users.noreply.github.com>
2023-07-17 06:19:25 +0900

    manual

--- 7_manual.sh
+++ 7_manual.sh
@@ -1,3 +1,3 @@
 # 1. move /media/lwh/lwh_backup1/etc to /etc
 # 5. turn off syncthing option : global anounce
-# 6. /home/lwh/phone/000_Personal/Linux/Distrobox 설정.md
+# 6. gedit "/home/lwh/phone/000_Personal/Linux/Distrobox 설정.md"
