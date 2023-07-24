58a5ea8d722251601da5bf2bfd7a06307397a0a1
ppp821203 <40662344+ppp821203@users.noreply.github.com>
2023-07-24 23:16:53 +0900

    distro

--- 7_manual.sh
+++ 7_manual.sh
@@ -1,6 +1,7 @@
 # 1. move /media/lwh/lwh_backup1/etc to /etc
 # 5. turn off syncthing option : global anounce
-# 6. gedit "/home/lwh/phone/000_Personal/Linux/Distrobox 설정.md"
+# 6. gedit "/home/lwh/phone/000_Personal/Linux/Distrobox 설정 - ubuntu.md"
+# 6. gedit "/home/lwh/phone/000_Personal/Linux/Distrobox 설정 - arch.md"
 
 # if gnome-terminal not work? excute locale setting
 # sudo dpkg-reconfigure locales
