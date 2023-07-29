27032abc0ac4bdacdce999ad4363be0c8f3fd358
ppp821203 <40662344+ppp821203@users.noreply.github.com>
2023-07-29 10:49:14 +0900

    firewall

--- 04_firewall.sh
+++ 04_firewall.sh
@@ -4,12 +4,8 @@ echo "============== [S] [ 4 / 6 ] Firewall =============="
 sudo ufw default deny incoming
 sudo ufw default deny outgoing
 
-#gsconnect
-sudo ufw allow 1714:1764/tcp
-sudo ufw allow out 1714:1764/tcp
-
-sudo ufw allow 1714:1764/udp
-sudo ufw allow out 1714:1764/udp
+sudo ufw allow from 192.168.0.0/24
+sudo ufw allow out to 192.168.0.0/24
 
 #http
 sudo ufw allow 80
@@ -45,27 +41,12 @@ sudo ufw allow out 5000:5002/udp
 sudo ufw allow 9663
 sudo ufw allow out 9663
 
-#syncthing
-sudo ufw allow 21027
-sudo ufw allow out to 192.168.31.0/24 port 21027
-
-sudo ufw allow 22000
-sudo ufw allow out to 192.168.31.0/24 port 22000
-
 #ping, traceroute enable
 sudo ufw allow out to any port 33434:33524 proto udp
-
-#RDP
-sudo ufw allow 3389
-sudo ufw allow out 3389
 
 #vnc
 # sudo ufw allow 5900
 # sudo ufw allow out 5900
-
-# ssh
-sudo ufw allow ssh
-sudo ufw allow out ssh
 
 #torrent port
 sudo ufw allow 51413
