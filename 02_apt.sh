68b7a80d8dbda8493254e6fc4281d2bbc7c7be6b
ppp821203 <40662344+ppp821203@users.noreply.github.com>
2023-08-10 06:51:53 +0900

    02_apt

--- 02_apt.sh
+++ 02_apt.sh
@@ -17,8 +17,21 @@ echo "============== [E] [ 2-1 / 6 ] Install - apt =============="
 
 echo "============== [S] [ 2-2 / 6 ] Install - apt =============="
 
+curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
+sudo install -o root -g root -m 644 microsoft.gpg /usr/share/keyrings/microsoft-archive-keyring.gpg
+sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/usr/share/keyrings/microsoft-archive-keyring.gpg] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
+
+sudo apt-get install apt-transport-https
+sudo apt-get update
+sudo apt-get install code # or code-insiders
+
+echo "============== [E] [ 2-2 / 6 ] Install - apt =============="
+
+
+echo "============== [S] [ 2-3 / 6 ] Install - apt =============="
+
 sudo apt update
 sudo apt install -y flatpak
 # sudo apt install -y gnome-software gnome-software-plugin-flatpak
 
-echo "============== [E] [ 2-2 / 6 ] Install - apt =============="
+echo "============== [E] [ 2-3 / 6 ] Install - apt =============="
