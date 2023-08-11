f5abc9bed8bdb55e7d99dc9e972eeac8c8331da3
ppp821203 <40662344+ppp821203@users.noreply.github.com>
2023-08-11 14:27:12 +0900

    remove fonts

--- 05_custom.sh
+++ 05_custom.sh
@@ -63,10 +63,10 @@ dconf load / < ~/git/init_debian/gnome-shell/dconf_backup
 
 echo "============== [E] [5-4 / 6] Dconf restore =============="
 
-echo "============== [S] [5-5 / 6] install font =============="
-mkdir ~/.fonts
-cp -r ./fonts/* ~/.fonts/
-echo "============== [S] [5-5 / 6] install font =============="
+# echo "============== [S] [5-5 / 6] install font =============="
+# mkdir ~/.fonts
+# cp -r ./fonts/* ~/.fonts/
+# echo "============== [S] [5-5 / 6] install font =============="
 
 echo "============== [S] [5-6 / 6] Set Sudo key file =============="
 
