0cc3eb4a2217426c4420daa2dc108d537aa22252
ppp821203 <40662344+ppp821203@users.noreply.github.com>
2023-09-01 19:36:09 +0900

    remove proxmox

--- 02_apt_normal.sh
+++ 02_apt_normal.sh
@@ -13,9 +13,4 @@ sudo apt-get install code # or code-insiders
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo apt -y install ./google-chrome-stable_current_amd64.deb
 
-# proxmox
-sudo sh -c 'echo "deb [arch=amd64] http://download.proxmox.com/debian/pve bookworm pve-no-subscription" > /etc/apt/sources.list.d/proxmox.list'
-sudo wget https://enterprise.proxmox.com/debian/proxmox-release-bookworm.gpg -O /etc/apt/trusted.gpg.d/proxmox-release-bookworm.gpg 
-sudo apt install proxmox-ve postfix open-iscsi
-
 echo "============== [E] [ 2-2 / 6 ] Install - apt =============="
