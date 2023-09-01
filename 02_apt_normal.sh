07019db6415501094d87866882c2097e25902717
ppp821203 <40662344+ppp821203@users.noreply.github.com>
2023-09-01 19:35:10 +0900

    proxmox

--- 02_apt_normal.sh
+++ 02_apt_normal.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "============== [S] [ 2-2 / 6 ] Install - apt =============="
-
+# vscode
 curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
 sudo install -o root -g root -m 644 microsoft.gpg /usr/share/keyrings/microsoft-archive-keyring.gpg
 sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/usr/share/keyrings/microsoft-archive-keyring.gpg] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
@@ -9,7 +9,13 @@ sudo apt-get install apt-transport-https
 sudo apt-get update
 sudo apt-get install code # or code-insiders
 
+# googlechrome
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo apt -y install ./google-chrome-stable_current_amd64.deb
+
+# proxmox
+sudo sh -c 'echo "deb [arch=amd64] http://download.proxmox.com/debian/pve bookworm pve-no-subscription" > /etc/apt/sources.list.d/proxmox.list'
+sudo wget https://enterprise.proxmox.com/debian/proxmox-release-bookworm.gpg -O /etc/apt/trusted.gpg.d/proxmox-release-bookworm.gpg 
+sudo apt install proxmox-ve postfix open-iscsi
 
 echo "============== [E] [ 2-2 / 6 ] Install - apt =============="
