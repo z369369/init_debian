65092e35ec0eb2a75eb9fa4c304bd4e061512fb6
ppp821203 <40662344+ppp821203@users.noreply.github.com>
2023-09-03 22:13:21 +0900

    man

--- 07_manual.sh
+++ 07_manual.sh
@@ -3,5 +3,18 @@
 # 6. gedit "/home/lwh/phone/000_Personal/Linux/Distrobox 설정 - ubuntu.md"
 # 6. gedit "/home/lwh/phone/000_Personal/Linux/Distrobox 설정 - arch.md"
 
+
+
 # if gnome-terminal not work? excute locale setting
 # sudo dpkg-reconfigure locales
+
+
+
+
+
+# distrobox-create -n ubuntu -i quay.io/toolbx-images/ubuntu-toolbox:22.04
+# distrobox-enter -n ubuntu
+# sudo apt install gnome-startup-applications
+# exit
+
+# distrobox-create -n arch -i quay.io/toolbx-images/archlinux-toolbox
