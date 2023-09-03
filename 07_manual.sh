7a67477c61a39e2ca7e90778d63363c499d92b8f
ppp821203 <40662344+ppp821203@users.noreply.github.com>
2023-09-04 00:49:36 +0900

    fix now

--- 07_manual.sh
+++ 07_manual.sh
@@ -7,14 +7,3 @@
 
 # if gnome-terminal not work? excute locale setting
 # sudo dpkg-reconfigure locales
-
-
-
-
-
-# distrobox-create -n ubuntu -i quay.io/toolbx-images/ubuntu-toolbox:22.04
-# distrobox-enter -n ubuntu
-# sudo apt install gnome-startup-applications
-# exit
-
-# distrobox-create -n arch -i quay.io/toolbx-images/archlinux-toolbox
