0f007021f2ab2e2c13eaa9cb774832cf88e6d3be
ppp821203 <40662344+ppp821203@users.noreply.github.com>
2023-09-10 03:57:01 +0900

    fupdate

--- link_Desktop/crontab/lwh_upgrade.sh
+++ link_Desktop/crontab/lwh_upgrade.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
+#fupdate
 flatpak update --noninteractive --assumeyes
 flatpak uninstall --unused --noninteractive --assumeyes
 
 rm -rf /home/lwh/.var/app/com.opera.Opera/config/opera/History
 
+#gupdate
 /home/lwh/.local/bin/gupdate > /dev/null 2>&1
