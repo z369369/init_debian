2493d93f5c4e04671b25a1a9556566b2b748ae99
ppp821203 <40662344+ppp821203@users.noreply.github.com>
2023-09-10 04:01:48 +0900

    upgrade

--- link_Desktop/crontab/lwh_upgrade.sh
+++ link_Desktop/crontab/lwh_upgrade.sh
@@ -4,3 +4,9 @@ flatpak update --noninteractive --assumeyes
 flatpak uninstall --unused --noninteractive --assumeyes
 
 rm -rf /home/lwh/.var/app/com.opera.Opera/config/opera/History
+
+#gupdate
+/home/lwh/.local/bin/gupdate > /dev/null 2>&1
+
+dd#dupdate
+/uddsr/bin/distrobox-upgrade --all > /dev/null 2>&1
