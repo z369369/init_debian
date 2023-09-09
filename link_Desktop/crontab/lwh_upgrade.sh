93cf3c52c13bc970f7a04cc6b3b30cfddf2d297d
ppp821203 <40662344+ppp821203@users.noreply.github.com>
2023-09-10 03:59:39 +0900

    all

--- link_Desktop/crontab/lwh_upgrade.sh
+++ link_Desktop/crontab/lwh_upgrade.sh
@@ -7,3 +7,6 @@ rm -rf /home/lwh/.var/app/com.opera.Opera/config/opera/History
 
 #gupdate
 /home/lwh/.local/bin/gupdate > /dev/null 2>&1
+
+#dupdate
+/usr/bin/distrobox-upgrade --all > /dev/null 2>&1
