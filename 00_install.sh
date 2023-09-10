cf8bccabae4e8d5efb7bad6e852d8ad9095a8b4a
ppp821203 <40662344+ppp821203@users.noreply.github.com>
2023-09-11 07:00:57 +0900

    install

--- 00_install.sh
+++ 00_install.sh
@@ -1,19 +1,4 @@
 #!/bin/bash
-# Check if the user is in the sudo group
-E_NOTROOT=87 # Non-root exit error.
-
-## check if is sudoer
-echo "============== [S] [ 1-1 / 6 ] Prepare =============="
-if ! $(sudo -l &> /dev/null); then
-    echo "  sudo permission need to run this script"
-    echo "  $(whoami) user should be in a sudo user group"
-    echo ""
-    echo "  su -"
-    echo "  adduser $(whoami) sudo"
-    echo ""
-    echo "  [FAIL] excute upper command and re-excute script "
-    exit $E_NOTROOT
-fi
 echo "============== [S] [ 1-1 / 6 ] Prepare =============="
 
 echo -e "GET http://google.com HTTP/1.0\n\n" | nc google.com 80 > /dev/null 2>&1
