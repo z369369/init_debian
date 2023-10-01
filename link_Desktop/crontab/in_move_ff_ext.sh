1336fd9986fb3739881cc2e6d9eff527abf1390e
ppp821203 <40662344+ppp821203@users.noreply.github.com>
2023-10-02 00:09:40 +0900

    fix now

---
+++ link_Desktop/crontab/in_move_ff_ext.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# Extract the folder names from the full paths
+if [[ $1 =~ ^.*[0-9]{4}-[0-9]{2}-[0-9]{2}.*$ ]]; then
+  	mv "/home/lwh/Downloads/$1" "/home/lwh/Downloads/Download_Home/firefox/"
+fi
+done
+
--- link_Desktop/crontab/lwh_upgrade.sh
+++ link_Desktop/crontab/lwh_upgrade.sh
@@ -6,7 +6,7 @@ flatpak uninstall --unused --noninteractive --assumeyes
 rm -rf /home/lwh/.var/app/com.opera.Opera/config/opera/History
 
 #gupdate
-/home/lwh/.local/bin/gupdate > /dev/null 2>&1
+#/home/lwh/.local/bin/gupdate > /dev/null 2>&1
 
 #dupdate
-/uddsr/bin/distrobox-upgrade --all > /dev/null 2>&1
+#/uddsr/bin/distrobox-upgrade --all > /dev/null 2>&1
