3ce722197acea04f9016e8d522a3d26bf36d4c43
ppp821203 <40662344+ppp821203@users.noreply.github.com>
2023-10-19 00:16:42 +0900

    metadata

--- link_gnome-shell/extensions/quick-settings-audio-panel@rayzeq.github.io/libs/widgets.js
+++ link_gnome-shell/extensions/quick-settings-audio-panel@rayzeq.github.io/libs/widgets.js
@@ -1,23 +1,56 @@
-const { GLib, GObject, Clutter, St } = imports.gi;
+const { St, GObject } = imports.gi;
 const { MixerSinkInput } = imports.gi.Gvc;
-const ByteArray = imports.byteArray;
 
 const PopupMenu = imports.ui.popupMenu; // https://gitlab.gnome.org/GNOME/gnome-shell/-/blob/main/js/ui/popupMenu.js
 const Volume = imports.ui.status.volume; // https://gitlab.gnome.org/GNOME/gnome-shell/-/blob/main/js/ui/status/volume.js
 
-const OutputStreamSlider = imports.ui.main.panel.statusArea.quickSettings._volume._output.constructor;
-const StreamSlider = Object.getPrototypeOf(OutputStreamSlider);
+const StreamSlider = imports.ui.main.panel.statusArea.quickSettings._volume._output.constructor;
 
-var ApplicationsMixer = class {
-    constructor(panel, index, filter_mode, filters) {
-        this.panel = panel;
+var QuickSettingsPanel = GObject.registerClass(
+    class QuickSettingsPanel extends St.BoxLayout {
+        constructor(options) {
+            const separated = options.separated;
+            delete options.separated;
 
-        // Empty actor used to know where to place sliders
-        const placeholder = new Clutter.Actor({ visible: false });
-        panel._grid.insert_child_at_index(placeholder, index);
+            super({
+                vertical: true,
+                style_class: separated ? " popup-menu-content quick-settings QSAP-panel-separated" : " QSAP-panel-merged",
+                ...options
+            });
+
+            this.hide();
+        }
+
+        add_child(widget) {
+            if (widget.visible) {
+                this.show();
+            }
+            widget._qsap_show_callack = widget.connect("show", () => this.show());
+            widget._qsap_hide_callack = widget.connect_after("hide", () => {
+                for (const child of this.get_children()) {
+                    if (child.visible)
+                        return;
+                }
+                this.hide();
+            });
+            super.add_child(widget);
+        }
+
+        remove_child(widget) {
+            widget.disconnect(widget._qsap_show_callack);
+            widget.disconnect(widget._qsap_hide_callack);
+            super.remove_child(widget);
+        }
+    }
+);
+
+// This class is a modified version of VolumeMixer from quick-settings-tweaks@qwreey
+var ApplicationsMixer = class ApplicationsMixer extends PopupMenu.PopupMenuSection {
+    constructor(filter_mode, filters) {
+        super();
+        this.actor.hide();
 
         this._sliders = {};
-        this._sliders_ordered = [placeholder];
         this._filter_mode = filter_mode;
         this._filters = filters.map(f => new RegExp(f));
 
@@ -52,137 +85,66 @@ var ApplicationsMixer = class {
             stream,
         );
         this._sliders[id] = slider;
-
-        this.panel.addItem(slider, 2);
-        this.panel._grid.set_child_above_sibling(slider, this._sliders_ordered.at(-1));
-
-        this._sliders_ordered.push(slider);
+        this.actor.add(slider);
+        this.actor.show();
     }
 
     _stream_removed(_control, id) {
-        if (!(id in this._sliders)) return;
+        if (id in this._sliders) {
+            this._sliders[id].destroy();
+            delete this._sliders[id];
+        }
 
-        this.panel.removeItem(this._sliders[id]);
-        this._sliders_ordered.splice(this._sliders_ordered.indexOf(this._sliders[id]), 1);
-        this._sliders[id].destroy();
-        delete this._sliders[id];
+        if (Object.keys(this._sliders).length === 0) {
+            this.actor.hide();
+        };
     }
 
     destroy() {
-        for (const slider of Object.values(this._sliders)) {
-            this.panel.removeItem(slider);
-            slider.destroy();
-        }
         this._sliders = null;
-
-        this._sliders_ordered[0].destroy();
-        this._sliders_ordered = null;
 
         this._mixer_control.disconnect(this._sa_event_id);
         this._mixer_control.disconnect(this._sr_event_id);
+
+        super.destroy();
     }
 };
 
-var ApplicationVolumeSlider = GObject.registerClass(class extends StreamSlider {
-    constructor(control, stream) {
-        super(control);
-        this.menu.setHeader('audio-headphones-symbolic', _('Output Device'));
+var ApplicationVolumeSlider = GObject.registerClass(
+    class ApplicationVolumeSlider extends StreamSlider {
+        constructor(control, stream) {
+            super(control);
 
-        try {
-            GLib.spawn_command_line_sync('pactl');
-        } catch (e) {
-            this._disable_pactl = true;
-        }
+            // This line need to be BEFORE this.stream assignement to prevent an error from appearing in the logs.
+            this._icons = [stream.name ? stream.name.toLowerCase() : stream.icon_name];
+            this.stream = stream;
+            // And this one need to be after this.stream assignement.
+            this._icon.fallback_icon_name = stream.icon_name;
 
-        if (!this._disable_pactl) {
-            this._control.connectObject(
-                'output-added', (_control, id) => this._addDevice(id),
-                'output-removed', (_control, id) => this._removeDevice(id),
-                'active-output-update', (_control, _id) => this._checkUsedSink(),
-                this
+            const vbox = new St.BoxLayout({ vertical: true });
+
+            const hbox = this.first_child; // this is the only child
+            const slider = hbox.get_children()[1];
+            hbox.remove_child(slider);
+            hbox.insert_child_at_index(vbox, 1);
+
+            const label = new St.Label({ natural_width: 0 });
+            label.style_class = "QSAP-application-volume-slider-label";
+            stream.bind_property_full('description', label, 'text',
+                GObject.BindingFlags.SYNC_CREATE,
+                (_binding, _value) => {
+                    return [true, this._get_label_text(stream)];
+                },
+                null
             );
-            // unfortunatly we don't have any signal to know that the active device changed
-            //stream.connect('', () => this._setActiveDevice());
-        
-            for (const sink of control.get_sinks()) {
-                // apparently it's possible that this function return null
-                const device = this._control.lookup_device_from_stream(sink)?.get_id();
-                if (device) {
-                    this._addDevice(device);
-                }
-            }
+
+            vbox.add(label);
+            vbox.add(slider);
         }
 
-        // This line need to be BEFORE this.stream assignement to prevent an error from appearing in the logs.
-        this._icons = [stream.name ? stream.name.toLowerCase() : stream.icon_name];
-        this.stream = stream;
-        // And this one need to be after this.stream assignement.
-        this._icon.fallback_icon_name = stream.icon_name;
-
-        if (!this._disable_pactl) {
-            this._checkUsedSink();
-        }
-
-        this._iconButton.y_expand = false;
-        this._iconButton.y_align = Clutter.ActorAlign.CENTER;
-
-        const box = this.child;
-        const sliderBin = box.get_children()[1];
-        box.remove_child(sliderBin);
-        const menu_button_visible = this._menuButton.visible;
-        box.remove_child(this._menuButton);
-
-        const vbox = new St.BoxLayout({ vertical: true, x_expand: true });
-        box.insert_child_at_index(vbox, 1);
-
-        const hbox = new St.BoxLayout();
-        hbox.add_child(sliderBin);
-        hbox.add_child(this._menuButton);
-        this._menuButton.visible = menu_button_visible; // we need to reset `actor.visible` when changing parent
-        // this prevent the tall panel bug when the button is shown
-        this._menuButton.y_expand = false;
-
-        const label = new St.Label({ natural_width: 0 });
-        label.style_class = "QSAP-application-volume-slider-label";
-        stream.bind_property_full('description', label, 'text',
-            GObject.BindingFlags.SYNC_CREATE,
-            (_binding, _value) => {
-                return [true, this._get_label_text(stream)];
-            },
-            null
-        );
-
-        vbox.add_child(label);
-        vbox.add_child(hbox);
-    }
-
-    _get_label_text(stream) {
-        const { name, description } = stream;
-        return name === null ? description : `${name} - ${description}`;
-    }
-
-    _checkUsedSink() {
-        let [, stdout, ,] = GLib.spawn_command_line_sync('pactl -f json list sink-inputs');
-        if (stdout instanceof Uint8Array)
-            stdout = ByteArray.toString(stdout);
-        stdout = JSON.parse(stdout);
-
-        for (const sink_input of stdout) {
-            if (sink_input.index === this.stream.index) {
-                const sink_id = this._control.lookup_device_from_stream(this._control.get_sinks().find(s => s.index === sink_input.sink))?.get_id();
-                if (sink_id) {
-                    this._setActiveDevice(sink_id);
-                }
-            }
+        _get_label_text(stream) {
+            const { name, description } = stream;
+            return name === null ? description : `${name} - ${description}`;
         }
     }
-
-    _lookupDevice(id) {
-        return this._control.lookup_output_id(id);
-    }
-
-    _activateDevice(device) {
-        GLib.spawn_command_line_async(`pactl move-sink-input ${this.stream.index} ${this._control.lookup_stream_id(device.stream_id).index}`);
-        this._setActiveDevice(device.get_id());
-    }
-});
+);
