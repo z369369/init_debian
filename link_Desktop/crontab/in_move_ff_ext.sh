1f6060b5fd14d60a169977da13dd755558d6d9d2
ppp821203 <40662344+ppp821203@users.noreply.github.com>
2023-10-24 16:06:24 +0900

    crontab

--- link_Desktop/crontab/in_move_ff_ext.sh
+++ link_Desktop/crontab/in_move_ff_ext.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 # Extract the folder names from the full paths
 if [[ $1 =~ ^.*[0-9]{4}-[0-9]{2}-[0-9]{2}.*$ ]]; then
+	sleep 3
+  	v1=$1
+  	t1=${v1:0:10}
+  	#echo "/home/lwh/Downloads/Download_Home/firefox/${t1}*" >> ~/log3.txt
+  	
+  	rm -rf /home/lwh/Downloads/Download_Home/firefox/${t1}*
+  	sleep 3
+  	
   	mv "/home/lwh/Downloads/$1" "/home/lwh/Downloads/Download_Home/firefox/"
 fi
 done
-
