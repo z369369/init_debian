e93061a079ab2d4f4da63658aecf73f03a0b5514
ppp821203 <40662344+ppp821203@users.noreply.github.com>
2024-01-28 16:13:56 +0900

    debian

--- link_Desktop/autostart/custom_execute.sh
+++ link_Desktop/autostart/custom_execute.sh
@@ -8,13 +8,16 @@
 cat ~/.key | sudo -S ufw default deny outgoing 
 cat ~/.key | sudo -S ufw default deny incoming
 cat ~/.key | sudo -S ufw enable
-
+#cat ~/.key | sudo usbreset 2563:0526
 #wmctrl -s 4
 gnome-terminal
 
 #wmctrl -s 3
 #nohup nautilus ~ 1> /dev/null 2>&1 &
 nohup firefox-esr 1> /dev/null 2>&1 &
+
+#/usr/bin/flatpak run --branch=stable --arch=x86_64 --command=steam com.valvesoftware.Steam steam://open/games
+
 #nohup syncthing --allow-newer-config --no-browser 1> /dev/null 2>&1 &
 disown
 
