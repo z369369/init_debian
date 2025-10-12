e871cf6d7e87ffad6daa331324386c550e539f0b
ppp821203 <40662344+ppp821203@users.noreply.github.com>
2025-10-12 12:21:14 +0900

    backup1 top backup

--- 98_manual.sh
+++ 98_manual.sh
@@ -1,4 +1,4 @@
-# 1. move /media/lwh/lwh_backup1/etc to /etc
+# 1. move /media/lwh/lwh_backup/etc to /etc
 # 5. turn off syncthing option : global anounce
 # 6. gedit "/home/lwh/phone/000_Personal/Linux/Distrobox 설정 - ubuntu.md"
 # 6. gedit "/home/lwh/phone/000_Personal/Linux/Distrobox 설정 - arch.md"
